5938d9be359a8083540c82f6ed86d8d6a7a431f5
Holger Mueller <9jholger@gmail.com>
2020-06-05 14:33:50 -0500

    got protag on dom

--- src/pages/indexComponents/protagDisplay.js
+++ src/pages/indexComponents/protagDisplay.js
@@ -1,22 +1,56 @@
-import React from "react"
+import React, { Component } from "react"
 import { Card, CardContent, CardActions, Button } from "@material-ui/core"
 
-const ProtagDisplay = () => (
-  <Card>
-    <CardContent>
-      <h1>Protag</h1>
-    </CardContent>
-    <CardActions>
-      <Button
-        style={{ marginLeft: "auto", marginRight: "auto" }}
-        aria-label="choose a protagonist"
-        size="large"
-        color="primary"
-      >
-        Choose
-      </Button>
-    </CardActions>
-  </Card>
-)
+export default class ProtagDisplay extends Component {
+  constructor(props) {
+    super(props)
 
-export default ProtagDisplay
+    this.state = {
+      protagList: [
+        "Wizard",
+        "Professor",
+        "Farmer",
+        "Student",
+        "Surfer",
+        "Shaman",
+      ],
+      chosenProtag: "",
+    }
+  }
+
+  hanndleClick = () => {
+    let protagChosenFromList = this.state.protagList[
+      Math.floor(Math.random() * this.state.protagList.length)
+    ]
+
+    this.setState({ chosenProtag: protagChosenFromList })
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardContent>
+          <h1
+            style={{ textAlign: "center" }}
+            name="chosenProtag"
+            id="protagDisplay"
+            value={this.state.chosenProtag}
+          >
+            {this.state.chosenProtag}
+          </h1>
+        </CardContent>
+        <CardActions>
+          <Button
+            style={{ marginLeft: "auto", marginRight: "auto" }}
+            aria-label="choose a protagonist"
+            size="large"
+            color="primary"
+            onClick={this.hanndleClick}
+          >
+            Choose
+          </Button>
+        </CardActions>
+      </Card>
+    )
+  }
+}
