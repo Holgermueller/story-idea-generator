f2c97c08aa45b21c190d50dcb67798c2b2b9668e
Holger Mueller <9jholger@gmail.com>
2020-06-07 09:58:59 -0500

    made random word display conditional

--- src/pages/indexComponents/randomWordDisplay.js
+++ src/pages/indexComponents/randomWordDisplay.js
@@ -1,22 +1,58 @@
-import React from "react"
+import React, { Component } from "react"
 import { Card, CardContent, CardActions, Button } from "@material-ui/core"
 
-const RandomWordDisplay = () => (
-  <Card>
-    <CardContent>
-      <h1>Random</h1>
-    </CardContent>
-    <CardActions>
-      <Button
-        style={{ marginLeft: "auto", marginRight: "auto" }}
-        aria-label="choose a random word"
-        size="large"
-        color="primary"
-      >
-        Choose
-      </Button>
-    </CardActions>
-  </Card>
-)
+export default class RandomWordDisplay extends Component {
+  constructor(props) {
+    super(props)
 
-export default RandomWordDisplay
+    this.state = {
+      randomWordList: ["cheese", "princess", "ocean"],
+      chosenRandomWord: "",
+    }
+  }
+
+  handleClick = () => {
+    let randomWordChosenFromList = this.state.randomWordList[
+      Math.floor(Math.random() * this.state.randomWordList.length)
+    ]
+
+    this.setState({ chosenRandomWord: randomWordChosenFromList })
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardContent>
+          {this.state.chosenRandomWord ? (
+            <h1
+              style={{ textAlign: "center" }}
+              name="chosenProtag"
+              id="protagDisplay"
+            >
+              {this.state.chosenRandomWord}
+            </h1>
+          ) : (
+            <h1
+              style={{ textAlign: "center" }}
+              name="chosenProtag"
+              id="protagDisplay"
+            >
+              Random
+            </h1>
+          )}
+        </CardContent>
+        <CardActions>
+          <Button
+            style={{ marginLeft: "auto", marginRight: "auto" }}
+            aria-label="choose a random word"
+            size="large"
+            color="primary"
+            onClick={this.handleClick}
+          >
+            Choose
+          </Button>
+        </CardActions>
+      </Card>
+    )
+  }
+}
